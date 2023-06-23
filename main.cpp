b69f515ca174c5b957f607c79019f6118337d3b7
cxnajder <cxnajder@github.com>
2023-06-24 01:17:00 +0200

    working variable declarations

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
         "PROGRAMING Principles and Practice using C++"
 
     Hist:
-        Reconstruction: cxnajder            summer 2023
-        Update: Bjarne Stroustrup    august 2007
+        Reconstruction: cxnajder        summer 2023
+        Update: Bjarne Stroustrup       august 2007
         ...
         Original author: Bjaorne Stroustrup
-            (bs@cs.tamu.edu)        spring 2004
+            (bs@cs.tamu.edu)            spring 2004
         
     This code implements calculator with basic expresions
     Imoprts input data form std::cin stream, outputs to std::cout
@@ -20,7 +20,7 @@
             print
             quit
 
-            contains: token_stream, variables
+            contains: token_stream
 
         Statement:
             expression
@@ -52,13 +52,17 @@
         number:
             floating-point-literal
 
+
+            global:
+                Variables and const chars/strs
+
 */
 
 #include <iostream>
 #include <string>
 #include <cmath>
 #include <vector>
-
+#include <cctype>
 
 void error(std::string s1, std::string s2 = ""){
     throw std::runtime_error(s1+s2);
@@ -101,11 +105,24 @@ const char vname = 'v';
 const std::string dec_key = "let";
 
 //TOKEN ******************************************************
-class Token{
+class Token {
 public:
     char kind;
     double value;
+    std::string varname;
+
+    Token();
+    Token(char ch);
+    Token(char ch, double val);
+    Token(char ch, std::string vn);
+
 };
+
+Token::Token() {}
+Token::Token(char ch) : kind(ch), value(0) {}
+Token::Token(char ch, double val) : kind(ch), value(val) {} 
+Token::Token(char ch, std::string vn) : kind(ch), varname(vn) {}
+
 //TOKEN_STREAM **********************************************
 class Token_stream{
 public:
@@ -118,7 +135,7 @@ private:
     bool full;
 };
 
-Token_stream::Token_stream(){}
+Token_stream::Token_stream() {}
 
 void Token_stream::putback(Token t){
     if (full) error("Cannot call putback() when buffer is full.");
@@ -158,7 +175,8 @@ Token Token_stream::get(){
     case '%':
     case '+': 
     case '-':
-        return Token{ch};
+    case '=':
+        return Token(ch);
     case '.':
     case '0': case '1': case '2': case '3': case '4':
     case '5': case '6': case '7': case '8': case '9':
@@ -166,10 +184,18 @@ Token Token_stream::get(){
             std::cin.putback(ch);
             double val;
             std::cin >> val;
-            return Token{number, val};
+            return Token(number, val);
         }
     default:
-        break;
+        if (std::isalpha(static_cast<unsigned char>(ch))) {
+            std::cin.putback(ch);
+            std::string s;
+            std::cin>>s;
+            if (s == dec_key)
+                return Token(let);
+            return Token(vname, s);
+        }
+        error("Invalid token.");
     }
 }
 
@@ -182,23 +208,29 @@ public:
 
 std::vector<Variable>  var_table;
 
+    //var_tab debug:
+void check_table(){
+    for (auto v: var_table)
+        std::cout<<v.name<<" = "<<v.value<<'\n';
+}
+
 double get_value(const std::string & s){
     for (int i = 0; i < var_table.size(); ++i)
-            if (var_table.at(i).name == s )
-                return var_table.at(i).value;
+            if (var_table[i].name == s )
+                return var_table[i].value;
     error("Accessing: undefined variable ", s);    
 }
 void set_value(const std::string & s, const double & d){
     for (int i = 0; i < var_table.size(); ++i)
-            if (var_table.at(i).name == s ){
-                var_table.at(i).value = d;
+            if (var_table[i].name == s ){
+                var_table[i].value = d;
                 return;
             }
     error("Changing: undefined variable ", s);    
 }
-bool is_declared (const std::string & var_name){
+bool is_declared (const std::string & varname){
     for (auto var: var_table)
-        if (var.name == var_name)
+        if (var.name == varname)
             return true;
     return false;
 }
@@ -208,6 +240,8 @@ void define_var(const std::string & name, const double & val){
     var_table.push_back(Variable{name, val});
     return;
 }
+
+
 
 //REST OF FUN ***************************************************************************
 double statement(Token_stream & ts);
@@ -234,12 +268,13 @@ double declaration(Token_stream & ts){
     Token t = ts.get();
     if (t.kind != vname)
         error("Expected name in declaration.");
-    std::string var_name = ""; //FIX LATER -- IMPROVE
+    std::string varname = t.varname; 
     t = ts.get();
     if (t.kind != '=')
         error("Missing '=' sign in declaration.");
     double d = expression(ts);
-    define_var(var_name, d);
+    define_var(varname, d);
+    return d;
 }
 
 double expression(Token_stream & ts){
@@ -334,6 +369,7 @@ void calculate() {
                 break;
             ts.putback(t);
             std::cout<<result<<statement(ts)<<'\n';
+            check_table(); // <-- DIsplaying List of variables each prompt.
         }
         catch(const std::exception& e)
         {
